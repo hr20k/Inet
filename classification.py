fc26659e87df8c110422b89600f2c794a09d8f11
Haruki <haruki.private@gmail.com>
2017-05-03 00:02:58 +0900

    add: load_inet_data()

--- classification.py
+++ classification.py
@@ -1,5 +1,19 @@
 import argparse
+import datetime
+import csv
 
+def load_inet_data(fname):
+    f = open(fname, 'r')
+    d = csv.reader(f)
+    i_data = []
+    for i in d:
+        append = str(i[0]) + '/' + str(i[1])
+        t = {'datetime': datetime.datetime.strptime(append, '%Y-%m-%d/%H:%M'),
+             'id1': i[2],
+             'id2': i[3]}
+        i_data.append(t)
+
+    return i_data
 def main():
     # 引数からファイル名を取得
     parser = argparse.ArgumentParser()
@@ -9,6 +23,7 @@ def main():
     print(options.input_csv_file)
     fname = options.input_csv_file
 
+    i_data = load_inet_data(fname)
 
 if __name__ == '__main__':
     main()
