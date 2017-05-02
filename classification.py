f9a5e28af9230b1b644570d28b3604eef6bcd4ce
Haruki <haruki.private@gmail.com>
2017-05-03 00:01:41 +0900

    add: 引数からファイル名を取得

---
+++ classification.py
@@ -0,0 +1,14 @@
+import argparse
+
+def main():
+    # 引数からファイル名を取得
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_csv_file")
+    parser.add_argument("--encoding", default="utf_8")
+    options = parser.parse_args()
+    print(options.input_csv_file)
+    fname = options.input_csv_file
+
+
+if __name__ == '__main__':
+    main()
